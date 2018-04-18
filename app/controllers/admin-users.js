ac57961fc31d4e18c5d6a50e607fada40cf3dd12
user <gianmarcogh20@gmail.com>
2018-04-18 10:09:37 +0200

    Gianmarco commit

--- app/controllers/admin-users.js
+++ app/controllers/admin-users.js
@@ -3,4 +3,19 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
     session: service(),
     currentUser: service(),
+    store: service(),
+    username: '',
+    password: '',
+    actions: {
+        storeuser(){
+            let user = this.get('store').createRecord('user',{
+               username: this.get('username'),
+               password: this.get('password') 
+            });
+            user.save();
+            //this.get('store').findAll('user').then(data => {
+              //this.set('data', data);
+            //});
+        },
+    },
 });
