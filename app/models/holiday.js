a9b1632e3a6b91831ee2816863eee8d43ed8d369
Siriwiri94 <irisbr94@hotmail.com>
2018-04-18 09:29:56 +0200

    Add holiday, my holidays, delete holiday

--- app/models/holiday.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-    start_day: DS.attr('date'),
-    end_day: DS.attr('date'),
-    accepted: DS.attr('boolean'),
-    user:DS.attr('number'), 
-});
--- app/models/vacation-request.js
+++ app/models/vacation-request.js
@@ -1,19 +1,30 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+
 export default DS.Model.extend({
+    currentUser: service(),
     startDay: DS.attr('date'),
     endDay: DS.attr('date'),
     accepted: DS.attr('boolean'),
     user: DS.belongsTo('user'),
-    userId: DS.attr('number'),
     vacationType: DS.belongsTo('vacationType'),
     state: computed('accepted', function(){
         if(this.get('accepted')===true){
-            return 'Aceptado'
+            return 'Accepted'
         }else if(this.get('accepted')===false){
-            return 'Rechazado'
+            return 'Rejected'
         }else if(this.get('accepted')==null){
-            return 'Pendiente'
+            return 'Pending'
         }
-    })
+    }),
+    totalDays: computed('startDay', 'endDay', function(){
+        var start= this.get("startDay").getTime();
+        var end= this.get("endDay").getTime();
+        var diff = end-start;
+        return diff/(1000*60*60*24);
+    }),
+    isMe: computed('user.id', 'currentUser.user.id', function(){
+        return this.get('user.id') === this.get('currentUser.user.id');
+    }),
 });
